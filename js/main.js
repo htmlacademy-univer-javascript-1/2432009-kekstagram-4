f57c8fbb5146dd33da7d573ba54cf3d101f907d0
Елизавета Ильина <pole.risa@mail.ru>
2023-12-11 23:07:01 +0500

    1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import { drawMiniatures } from './small-pictures.js';
 import { getPhotoDescription } from './data.js';
+import { initEditPopup } from './edit-popup.js';
+
+
 const renderedPhotos = getPhotoDescription();
 drawMiniatures(renderedPhotos);
+initEditPopup();
